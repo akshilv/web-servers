e72e09164f92a83784858027181879d2f4684f99
i341668 <akshil.verma@sap.com>
2019-04-25 14:10:51 +0530

    Add Vuetify
    - Add Vuetify to the package

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
 new Vue({
   router,
